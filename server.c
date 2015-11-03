9ff4659bec9db2e0a80c31d5ac0fea1909d1a04e
root <root@localhost.localdomain>
2015-11-04 03:26:39 +0400

    sem server

--- server.c
+++ server.c
@@ -9,7 +9,6 @@
 #include <unistd.h>
 
 #define THREADS_AMNT 2
-static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 int threads_used[THREADS_AMNT];
 
 typedef struct DATA_T
@@ -34,57 +33,45 @@ typedef struct mymsgbuf_out
 typedef struct thread_input
 {
 	mybuf_in_t buf_in;
-	key_t key;
 	int thread_num;
+	int msgid;
+	int semid;
 }thread_t;
 
 void* my_thread(void* dummy) 
 {
-    int msquid = 0;
     thread_t* input = (thread_t*)dummy; 
     
     mybuf_out_t output = {};
     
+    struct sembuf destroy_thread = {};
+   
+    destroy_thread.sem_num = 0;													
+    destroy_thread.sem_op =  1;												
+    destroy_thread.sem_flg = 0;
+    
     output.mtype = input -> buf_in.data.my_id;
     
     output.result = input -> buf_in.data.num_one * input -> buf_in.data.num_two;
-    
-    /*
-     * передали бы сюда сразу msguid
-     */
-    if((msquid = msgget(input -> key, 0666 | IPC_CREAT)) < 0)
-	{
-		printf("Can\'t get msquid\n");
-		exit(-1);
-	}
-     
-    /*
-     * Гарантируется, что для очередей сообщений методы msgsnd и msgrcv потокобезопасны (thread-safety), т.е. можно спокойно вызывать её из нескольких потоков или процессов 
-     * и они отработают корректно.
-     */
-    
-    pthread_mutex_lock(&mutex); 
-    
-    if(msgsnd(msquid, (mybuf_out_t *) &output, sizeof(int), 0) < 0)
+             
+    if(msgsnd(input -> msgid, (mybuf_out_t *) &output, sizeof(int), 0) < 0)
     {
       printf("Can\'t send message to queue\n");
-      msgctl(msquid, IPC_RMID, (struct msqid_ds*)NULL);
-      pthread_mutex_unlock(&mutex);
+      msgctl(input -> msgid, IPC_RMID, (struct msqid_ds*)NULL);
       exit(-1);
     }
-   
-   pthread_mutex_unlock(&mutex);
-
-   input -> thread_num = 0;
-   
+    
    threads_used[input -> thread_num] = 0;
    
+   semop(input -> semid, &destroy_thread, 1);
+ 
    return NULL;
 }
 
 int main()
 {
 	int msquid = 0; 
+	int semid = 0;
 	int result = 0;
 	int successfull_thread_creation = 0;
 	key_t key = 0; 
@@ -92,13 +79,33 @@ int main()
 	char pathname[] = "ex0.c"; 
 		
 	pthread_t thread_id;
-	thread_t * funk_argument = (thread_t * )calloc(THREADS_AMNT, sizeof(thread_t));
+	thread_t * func_argument = (thread_t * )calloc(THREADS_AMNT, sizeof(thread_t));
 
 	if((key = ftok(pathname, 0)) < 0)
 	{
 		printf("Can\'t generate key\n");
 		exit(-1);
 	}
+	
+    if((semid = semget(key, 1, 0666 | IPC_CREAT)) < 0)
+    {
+      printf("Can\'t get msqid\n");
+      exit(-1);
+    }
+    
+    struct sembuf initialize_sem = {};
+   
+    initialize_sem.sem_num = 0;													
+    initialize_sem.sem_op =  THREADS_AMNT + 1;												
+    initialize_sem.sem_flg = 0;
+    
+    struct sembuf are_there_free_threads = {};
+  
+    are_there_free_threads.sem_num = 0;													
+    are_there_free_threads.sem_op =  -1;													
+	are_there_free_threads.sem_flg = 0;
+    
+    semop(semid, &initialize_sem, 1);
 
 	if((msquid = msgget(key, 0666 | IPC_CREAT)) < 0)
 	{
@@ -109,8 +116,7 @@ int main()
 	mybuf_in_t mybuf_input = {};
 	
 	while(1)
-	{  
-		
+	{  		
 		if ((msgrcv(msquid, (mybuf_in_t *) &mybuf_input, sizeof(mydata_t), 0, 0)) < 0)
 		{
 			printf("Can\'t receive message from queue\n");
@@ -120,35 +126,24 @@ int main()
 		{
 			printf("I recieved message! Process id is %d\n", mybuf_input.data.my_id);
 			sleep(20);
-      /* 
-       * FIXIT: 
-       * Вот что никогда-никогда не нужно делать, так это использовать label`ы. Это считается дурным тоном.
-       * С помощью буквально пары меток можно настолько запутать логику работы программы, что отладить её будет достаточно сложно.
-       */
-loop:
 
 			if(mybuf_input.mtype == 255)
 				exit(0);
+				
+			semop(semid, &are_there_free_threads, 1);
 
-      /*
-       * В целом, это не самое изящное и хорошее решение, т.к. ваш ожидающий цикл из меток будет "съедать" 100% процессорного времени.
-       * Решение с семафорами лучше. Если вам свой вариант нравится больше, то оставьте его.
-       */
-			for(int i = 0; i < THREADS_AMNT; i++)
+   			for(int i = 0; i < THREADS_AMNT; i++)
 			{
 				if(threads_used[i] == 0)
 				{	
 					threads_used[i] = 1;
-					successfull_thread_creation = 1;
 					
-          /*
-           * function -> func?
-           */
-					funk_argument[i].buf_in = mybuf_input;
-					funk_argument[i].thread_num = i;
-					funk_argument[i].key = key;
+					func_argument[i].buf_in = mybuf_input;
+					func_argument[i].thread_num = i;
+					func_argument[i].msgid = msquid;
+					func_argument[i].semid = semid;
 										
-					result = pthread_create(&thread_id, (pthread_attr_t *)NULL, my_thread, funk_argument + i);
+					result = pthread_create(&thread_id, (pthread_attr_t *)NULL, my_thread, func_argument + i);
 
 					if (result) 
 					{
@@ -159,11 +154,6 @@ loop:
 					break;
 				}
 			}	
-			
-			if(successfull_thread_creation == 1)	
-				successfull_thread_creation = 0;
-			else
-				goto loop;
 		}
 	}
   
