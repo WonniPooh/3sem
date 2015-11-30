324eeafd35dd3c2e681f0a734a71657f79f1292d
aligarh09 <aligarh09@mail.ru>
2015-11-30 21:54:29 +0300

    SIGINT & user_handler

---
+++ signals.c
@@ -0,0 +1,16 @@
+#include <signal.h>
+#include <stdio.h>
+
+void* my_handler(int nsig) 
+{
+	printf("HAHA! You cannot quit!\n");
+}
+
+int main(void)
+{
+(void)signal(SIGINT, SIG_IGN);
+(void)signal(SIGQUIT, my_handler);
+
+while(1);
+return 0;
+}
