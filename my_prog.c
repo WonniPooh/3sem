f44c993817e6e61821696b947e52aa7b00723d01
Alexey <aligarh09@mail.ru>
2015-12-08 02:21:50 +0300

    Labels deleted.

--- my_prog.c
+++ my_prog.c
@@ -12,35 +12,29 @@ int main()
   scanf("%d", &nodes_amnt);
   pid_t pid = fork();
   int wait_status = 0;
-
-  /*
-   * FIXIT:
-   * Label`ы не надо использовать никогда. Совсем-совсем никогда.
-   * С их помощью можно совершенно запутать логику программы.
-   * Можно реализовать любой алгоритм без их использования.
-   */
   
-loop:   
-  if(pid == 0)
+  for(current_fork_lvl = 1; current_fork_lvl <= nodes_amnt; current_fork_lvl++)
   {
-     printf("I am child with id %d of pr. with UID %d, current fork lvl is %d\n", getpid(), getppid(), current_fork_lvl);
-     current_fork_lvl++;
-     if(current_fork_lvl < nodes_amnt)
-     {
-        pid = fork();
-        goto loop;
-     }
-     else
-     {
-         printf("I am the last child with id %d of pr. with UID %d, exiting with a code %d\n", getpid(), getppid(), 0);
-         exit(0);
-     }
-  }
-  else 
-  {
-     wait(&wait_status);
-     printf("I am node with id %d, current fork lvl is %d\n", getpid(), current_fork_lvl);
-     printf("Exiting with a code %d\n", 0);
-     exit(0);
+    if(pid == 0)
+    {
+       printf("I am child with id %d of pr. with UID %d, current fork lvl is %d\n", getpid(), getppid(), current_fork_lvl);
+       
+       if(current_fork_lvl < nodes_amnt)
+       {
+          pid = fork();
+       }
+       else
+       {
+           printf("I am the last child with id %d of pr. with UID %d, exiting with a code %d\n", getpid(), getppid(), 0);
+           exit(0);
+       }
+    }
+    else 
+    {
+       wait(&wait_status);
+       printf("I am node with id %d, current fork lvl is %d\n", getpid(), current_fork_lvl);
+       printf("Exiting with a code %d\n", 0);
+       exit(0);
+    }
   }
 }
